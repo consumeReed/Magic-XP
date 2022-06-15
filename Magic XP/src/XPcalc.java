e1eaeff48e8f3955c5a52d0bfc1ff9ee8c880853
consumeReed <rmitchell122w@gmail.com>
2022-06-15 19:20:30 -0400

    changes

---
+++ Magic XP/src/XPcalc.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class XPcalc {
+
+	Map<String, String> xpLvl = new HashMap<String, String>();
+	
+	public XPcalc()
+	{
+		BufferedReader br = null;
+		try {
+			File f = new File("lib\\xp.txt");
+			br = new BufferedReader(new FileReader(f));
+			String line = null;
+			while((line = br.readLine()) != null)
+			{
+				String[] parts = line.split(":");
+				
+				String lvl = parts[0].trim();
+				String xp = parts[1].trim();
+				xpLvl.put(lvl, xp);
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public String getXP(String lv)
+	{
+		return xpLvl.get(lv);
+	}
+	
+}
